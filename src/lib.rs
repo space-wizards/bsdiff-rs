18399cd968999a47ae225afb7203f190e91b2f58
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-06-22 15:05:39 +0200

    Revert bsdiff changes. They broke badly.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ extern crate byteorder;
 pub mod bsdiff;
 pub mod patch;
 
-use std::io::Cursor;
+use std::slice;
+use std::io::{Cursor, Write};
 
 #[test]
 fn test_it() {
@@ -14,7 +15,7 @@ fn test_it() {
     let two = vec![1];
 
     let mut cursor = Cursor::new(Vec::new());
-    bsdiff::bsdiff(&one, &two, &mut cursor).unwrap();
+    unsafe {bsdiff::bsdiff(one.as_ptr(), one.len() as isize, two.as_ptr(), two.len() as isize, &mut cursor, test_write);}
 
     cursor.set_position(0);
 
@@ -25,4 +26,14 @@ fn test_it() {
     let mut patched = vec![0];
     patch::bspatch(&one, &mut cursor, &mut patched).unwrap();
     assert_eq!(patched, two);
+}
+
+#[cfg(test)]
+unsafe extern "C" fn test_write(writer: &mut Cursor<Vec<u8>>, buffer: *const libc::c_void,
+    size: libc::c_int,
+) -> libc::c_int {
+    match writer.write(slice::from_raw_parts(buffer as *mut u8, size as usize)) {
+        Ok(x) => if x == size as usize {0} else {-1},
+        Err(_) => -1 
+    }
 }
